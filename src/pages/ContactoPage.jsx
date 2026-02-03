d8a141501a31459baa9b7d34a3fa21d1f3c3308e
Reynaldo003 <rvallejo276@gmail.com>
2026-02-03 15:18:12 -0600

    correccion contacto

--- src/pages/ContactoPage.jsx
+++ src/pages/ContactoPage.jsx
@@ -1,6 +1,6 @@
 // src/pages/ContactoPage.jsx
 import SobreYMapa from "@/components/SobreYMapa";
-import { CLINIC, PRIMARY } from "../shared/ClinicData";
+import { CLINIC, PRIMARY } from "../shared/clinicData";
 
 export default function ContactoPage() {
     return <SobreYMapa CLINIC={CLINIC} PRIMARY={PRIMARY} />;
